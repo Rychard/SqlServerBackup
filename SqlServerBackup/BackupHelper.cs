f0bb096855cf009383511d148c21d5aa8288ae91
Joshua Shearer <joshua.a.shearer@gmail.com>
2014-06-01 14:28:43 -0400

    Initial commit of project code.

---
+++ SqlServerBackup/BackupHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data.SqlClient;
+using Microsoft.SqlServer.Management.Smo;
+
+namespace SqlServerBackup
+{
+    public static class BackupHelper
+    {
+        public static void BackupDatabase(Server server, String databaseName, String destinationPath, int completionCallbackInterval, Action<int, String> completionCallback, Action<SqlError> errorCallback)
+        {
+            String backupSetName = String.Format("EnsembleBackup_{0}", databaseName);
+            String backupSetDescription = "User-requested backup for Self-Service Tools";
+
+            //Define a Backup object variable. 
+            Backup bk = new Backup
+            {
+                Action = BackupActionType.Database,
+                BackupSetDescription = backupSetDescription,
+                BackupSetName = backupSetName,
+                Database = databaseName,
+                Incremental = false, // This is a full database backup.
+                ExpirationDate = System.DateTime.UtcNow.AddYears(-1), // Already expired.  Allows us to overwrite them easily with subsequent backups.
+                LogTruncation = BackupTruncateLogType.NoTruncate, // I'm not sure what the implications of truncating the log are, so don't do that.
+                CopyOnly = true,
+                PercentCompleteNotification = completionCallbackInterval,
+                Initialize = true,
+            };
+
+            // We're going to save this backup as a file. 
+            String backupDeviceName = String.Format(destinationPath, databaseName);
+            BackupDeviceItem bdi = new BackupDeviceItem(backupDeviceName, DeviceType.File);
+            bk.Devices.Add(bdi);
+
+            //bk.Information += (sender, args) => { bk.Devices.Remove(bdi); errorCallback(args.Error); };
+            bk.PercentComplete += (sender, args) => { bk.Devices.Remove(bdi); completionCallback(args.Percent, args.Message); };
+
+            bk.SqlBackup(server);
+            Console.WriteLine("Done");
+        }
+    }
+}
---
+++ SqlServerBackup/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data.SqlClient;
+using Microsoft.SqlServer.Management.Smo;
+
+namespace SqlServerBackup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = new Options();
+            var results = CommandLine.Parser.Default.ParseArguments(args, options);
+
+            // Exit if arguments couldn't be parsed.
+            if (!results) { return; }
+
+            Console.WriteLine("Arguments Parsed!");
+
+            //Connect to the local, default instance of SQL Server. 
+            var _server = new Server(@".\SQLExpress");
+
+            foreach (Database database in _server.Databases)
+            {
+                if (database.Name == "tempdb") { continue; } // Backup and Restore operations are not permitted on tempdb
+                BackupHelper.BackupDatabase(_server, database.Name, String.Format("D:\\{0}.bak", database.Name), 1, CompletionCallback, ErrorCallback);
+            }
+        }
+
+        private static void ErrorCallback(SqlError sqlError)
+        {
+            Console.WriteLine(sqlError.Message);
+        }
+
+        private static void CompletionCallback(int i, string s)
+        {
+            Console.WriteLine("{0}% - {1}", i, s);
+        }
+    }
+
+    
+}
